3f653ffbd5db140558fdde9c6b06d7b11be61ddf
CodingKomodo <ecao7777@gmail.com>
2024-01-09 16:55:43 -0800

    Added PA1 Starter Code

---
+++ PA1/ProgrammingChallenges.java
@@ -0,0 +1,106 @@
+/*
+ * Name: TODO First Last
+ * PID:  TODO AXXXXXXXX
+ */
+
+/**
+ * Various Programming Challenges
+ *
+ * @author TODO
+ * @since TODO
+ */
+
+public class ProgrammingChallenges {
+
+    /**
+     * Problem 1
+     * TODO
+     */
+    public static boolean store(String item, float inWallet, float needed) {
+        // TODO
+        return false;
+    }
+    
+    /**
+     * Problem 2
+     * TODO
+     */
+    public static boolean compareArrays(int[] arr1, int[] arr2) {
+        // TODO
+        return false;
+    }
+
+    /**
+     * Problem 3
+     * TODO
+     */
+    public static int countNumbers(int[] arr1) {
+        // TODO
+        return 0;
+    }
+
+
+    /**
+     * Problem 4
+     * TODO
+     */
+    public static float[] positiveAverage(int[] arr1) {        
+        // TODO
+        return null;
+    }
+    
+    /**
+     * Problem 5
+     * TODO
+     */
+    public static boolean sameDigitFirstAndLast(int num1, int num2) {
+        // TODO
+        return false;
+    }
+
+
+    /**
+     * Problem 6
+     * TODO
+     */
+    public static boolean decreasingOrder(int[] elems) {
+        // TODO
+        return false;
+    }
+     
+    /**
+     * Problem 7
+     * TODO
+     */
+    public static void replaceMainDiagonal(int[][] elems) {
+        // TODO
+    }
+
+    /**
+     * Problem 8
+     * TODO
+     */
+    public static float averageGrade(int[][] grades, int index, boolean choice) {
+        // TODO
+        return 0f;
+    }
+
+    /**
+     * Problem 9
+     * TODO
+     */
+    public static String noDots(String str) {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Problem 10
+     * TODO
+     */
+    public static int[] twoElements(int[] elems) {
+        // TODO
+        return null;
+    }
+   
+}
