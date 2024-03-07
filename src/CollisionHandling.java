3db7bb45cda09448f7419b7944893a2fe1ea07f9
AhmedMostafa-147 <ahmad_hatem_2004@icloud.com>
2024-03-07 12:16:39 -0800

    src folder

---
+++ src/CollisionHandling.java
@@ -0,0 +1,45 @@
+/**
+ * PA8 Part 1 worksheet (style not required)
+ */
+public class CollisionHandling {
+
+    /**
+     * method that stores the result of Q1 in PA8 worksheet
+     * 4 insertions and then all of them
+     * @return hashtable representation after insertions
+     */
+    public static int[][] linearProbingResult(){
+        /*
+        TODO: create the values for the hashtable representations
+         */
+        int[][] output = null; //change
+        return output;
+    }
+
+    /**
+     * method that stores the result of Q2 in PA8 worksheet
+     * 4 insertions and then all of them
+     * @return hashtable representation after insertions
+     */
+    public static int[][] quadraticProbingResult(){
+        /*
+        TODO: create the values for the hashtable representations
+         */
+        int[][] output = null; //change
+        return output;
+    }
+
+    /**
+     * method that stores the result of Q3 in PA8 worksheet
+     * 4 insertions and then all of them
+     * @return hashtable representation after insertions
+     */
+    public static int[][] doubleHashingResult(){
+        /*
+        TODO: create the values for the hashtable representations
+         */
+        int[][] output = null; //change
+        return output;
+    }
+
+}
